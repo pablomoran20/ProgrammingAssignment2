07727f78d0042627f4e5fa01f46c1074539476f5
Pablo <pablomoran20@gmail.com>
2016-01-12 16:06:42 +0000

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -41,20 +41,3 @@ cacheSolve <- function(x, ...) {
   x$setinverse(z)
   z
 }
-=======
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
